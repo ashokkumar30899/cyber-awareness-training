3e1723a74224634e3ad491204daf6fbb6f0ad31b
Ashok Kumar <ashokkumar30899@gmail.com>
2026-02-06 13:14:48 +0530

    Add React cybersecurity awareness app with modules, quiz and results

--- src/authConfig.js
+++ src/authConfig.js
@@ -2,7 +2,7 @@ export const msalConfig = {
   auth: {
     clientId: "3f67225d-550a-43b3-8dc6-7376cda88059",
     authority: "https://login.microsoftonline.com/893db92d-2b9c-400b-b342-d7e0fb5a80ad",
-    redirectUri: "https://employeetraining.wissda.com"
+    redirectUri: "https://employeetraining.wissda.com/home"
   },
   cache: {
     cacheLocation: "sessionStorage",
